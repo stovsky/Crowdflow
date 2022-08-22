f0eef50194a2fb123f10dff641c35407d6d2bbc4
stovsky <tylerstovsky@gmail.com>
2022-08-22 15:56:47 -0400

    Initial change to the dashboard: 	Added the Google Maps API 	Search bar which uses Places API - logs to the console data of places based on search **requires installation: yarn add @react-google-maps/api**

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,16 +1,91 @@
-import React from 'react'
+import {
+  GoogleMap,
+  StandaloneSearchBox,
+  useJsApiLoader,
+} from '@react-google-maps/api'
+import { useState, useRef, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Button from 'components/Button'
 import { actions } from 'slices/app.slice'
 import { images } from 'theme'
 import styles from './dashboard.module.scss'
 
+const libraries = ['places']
+const mapContainerStyle = {
+  width: '80vw',
+  height: '100vh',
+}
+const center = {
+  lat: 41.499321,
+  lng: -81.694359,
+}
+
 const Dashboard = () => {
+  const [searchBox, setSearchBox] = useState(null)
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+
+  const mapRef = useRef()
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+  }, [])
+
+  const searchRef = useRef()
+
   const dispatch = useDispatch()
   const { me } = useSelector((state) => state.app)
+
+  if (loadError) return 'Error loading maps.'
+  if (!isLoaded) return 'Loading...'
+
+  const handleLoad = () => {
+    const search = new window.google.maps.places.SearchBox(searchRef.current)
+    setSearchBox(search)
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.container}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+          onLoad={onMapLoad}
+        >
+          <StandaloneSearchBox
+            onLoad={handleLoad}
+            onPlacesChanged={() =>
+              searchBox ? console.log(searchBox.getPlaces()) : null
+            }
+          >
+            <input
+              ref={searchRef}
+              id="search-box"
+              type="text"
+              placeholder="Find a place"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: 'absolute',
+                left: '50%',
+                marginLeft: '-120px',
+                top: '2%',
+              }}
+            />
+          </StandaloneSearchBox>
+        </GoogleMap>
         <img src={images.logo} className={styles.logo} alt="logo" />
         <h3 className={styles.greeting}>{`Hi👋, ${me?.fullName || 'User'}`}</h3>
         <h1 className={styles.title}>React + Firebase Boilerplate</h1>
