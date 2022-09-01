baf5ae39f7c617d99bb40a7b76a229d653bba842
Luke <77371718+ldalton02@users.noreply.github.com>
2022-08-31 22:19:06 -0700

    Makes map fullscreen cause I like it like that

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -51,8 +51,8 @@ const getColor = (place) => {
 
 const libraries = ['places']
 const mapContainerStyle = {
-  width: '80vw',
-  height: '100vh',
+  width: '100%',
+  height: '100%',
 }
 const center = {
   lat: 34.0689,
